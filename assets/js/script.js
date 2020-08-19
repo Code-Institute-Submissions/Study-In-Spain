abff3cdf244cbcf52a77fe59f52690ff1c7c0df3
hfzespinoz4 <harvin.zuniga@gmail.com>
2020-08-19 23:17:31 +0200

    Added EmailJS funcionallity to contact form at Index.html. Email functionallity has been verified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,42 @@
+const sendButton = document.getElementById("send-message");
 let senderName = document.getElementById("name");
 let senderEmail = document.getElementById("email");
 let senderSubject = document.getElementById("subject");
 let senderMessage = document.getElementById("message");
 
+
 function sendMail(){
-    emailjs.send("gmail", "", {
+    emailjs.send("gmail", "studyinspain", {
         "email-name": senderName.value,
         "email-address": senderEmail.value,
         "email-subject": senderSubject.value,
         "email-message": senderMessage.value,
         "email-copy": senderEmail.value
     })
+    .then (function (response){
+        $("#contact-notification").css("visibility", "visible").append(`
+            <div class="alert alert-success" role="alert"> 
+                Your email has been successfully sent, you´ll receive a copy.
+            </div>
+        `);
+        $('form :input').val('');
+        console.log("SUCCESS", response);
+    },
+    function (error){
+        $('#contact-notification').css("visibility", "visible").append(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                There is the following error with your request: ${error}.
+                Please, refresh this page or try again later
+            </div>
+        `)
+        console.log("FAILED", error)
+    });
+    return false;
+}
+
+
+sendButton.addEventListener('click', function (){
+    event.preventDefault();
+    sendMail();
+})
+
